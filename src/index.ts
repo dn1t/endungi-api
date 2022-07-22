dee87403e241d5ca4c6fd54c94edb958f09730f6
thoratica <me@tica.fun>
2022-07-23 01:31:17 +0900

    w

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 import express from 'express';
+import { join } from 'path';
 import userRouter from './routers/user';
 
 const app = express();
@@ -7,5 +8,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use('/api/user', userRouter);
+
+app.get('*', (_, res) => res.sendFile(join(__dirname, '../public/index.html')));
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
